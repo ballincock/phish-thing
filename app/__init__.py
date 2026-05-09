7bea80ad27237272270d2650e348e84db4a7cfbc
ballincock <zwilliams214@outlook.com>
2026-05-09 15:33:47 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,14 @@ from app.blueprints.dashboard import dash_bp
 from app.blueprints.gallery import gallery_bp
 from app.blueprints.user import user_bp
 from app.blueprints.security import security_bp
+from app.blueprints.community import community_bp
+from app.blueprints.gallery_profile import gallery_profile_bp
 
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
     app.config.from_object(Config)
+
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
     db.init_app(app)
 
@@ -37,7 +41,7 @@ def create_app():
             "blob:", 
             "http://localhost" 
         ],
-        'worker-src': ["'self'", "blob:"]
+        'worker-src': ["'self'", "blob:"] 
     }
 
     Talisman(app, content_security_policy=csp)
@@ -47,6 +51,8 @@ def create_app():
     app.register_blueprint(gallery_bp)
     app.register_blueprint(user_bp)
     app.register_blueprint(security_bp)
+    app.register_blueprint(community_bp)
+    app.register_blueprint(gallery_profile_bp)
 
     with app.app_context():
         db.create_all()
