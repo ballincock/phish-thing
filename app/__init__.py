c32ce305632c0c5ba18e84e054e4a5e23a5ee29e
ballincock <zwilliams214@outlook.com>
2026-05-09 01:10:50 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from app.models.user import db
 from app.blueprints.auth import auth_bp
 from app.blueprints.dashboard import dash_bp
 from app.blueprints.gallery import gallery_bp
+from app.blueprints.user import user_bp
 
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
@@ -14,14 +15,29 @@ def create_app():
 
     csp = {
         'default-src': "'self'",
-        'script-src': ["'self'", "https://pyscript.net", "'unsafe-inline'", "'unsafe-eval'"],
-        'style-src': ["'self'", "https://pyscript.net", "'unsafe-inline'"]
+        'script-src': [
+            "'self'", 
+            "https://pyscript.net", 
+            "'unsafe-inline'", 
+            "'unsafe-eval'"
+        ],
+        'style-src': [
+            "'self'", 
+            "https://pyscript.net", 
+            "'unsafe-inline'"
+        ],
+        'connect-src': [
+            "'self'", 
+            "https://pyscript.net"
+        ],
+        'img-src': ["'self'", "data:", "blob:"]
     }
     Talisman(app, content_security_policy=csp)
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(dash_bp)
     app.register_blueprint(gallery_bp)
+    app.register_blueprint(user_bp)
 
     with app.app_context():
         db.create_all()
