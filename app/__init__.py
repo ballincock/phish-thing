695fee539e68bc0b69cb63671c4eb75e652ff695
ballincock <zwilliams214@outlook.com>
2026-05-08 23:37:11 -0500

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_talisman import Talisman
+from config import Config
+from app.models.user import db
+from app.blueprints.auth import auth_bp
+from app.blueprints.dashboard import dash_bp
+from app.blueprints.gallery import gallery_bp
+
+def create_app():
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    csp = {
+        'default-src': "'self'",
+        'script-src': ["'self'", "https://pyscript.net", "'unsafe-inline'", "'unsafe-eval'"],
+        'style-src': ["'self'", "https://pyscript.net", "'unsafe-inline'"]
+    }
+    Talisman(app, content_security_policy=csp)
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(dash_bp)
+    app.register_blueprint(gallery_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
