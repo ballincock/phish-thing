594aaf0c1f5dd7242b326458ee366b834a1552d6
ballincock <zwilliams214@outlook.com>
2026-05-09 01:11:11 -0500

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -12,3 +12,20 @@ class User(db.Model):
     security_answer_hash = db.Column(db.String(256), nullable=False)
     backup_email = db.Column(db.String(120))
     mnemonic = db.Column(db.String(255), nullable=False)
+
+    # NEW COLUMNS
+    display_name = db.Column(db.String(100))
+    bio = db.Column(db.Text)
+    profile_picture = db.Column(db.String(255), default='uploads/profiles/default.png')
+    reddit = db.Column(db.String(100))
+    twitter = db.Column(db.String(100))
+    instagram = db.Column(db.String(100))
+    youtube = db.Column(db.String(100))
+    facebook = db.Column(db.String(100))
+    favorite_species = db.Column(db.String(100))
+    favorite_pb = db.Column(db.String(100))
+    fishing_region = db.Column(db.String(100))
+
+    def set_password(self, password):
+        from werkzeug.security import generate_password_hash
+        self.password_hash = generate_password_hash(password)
