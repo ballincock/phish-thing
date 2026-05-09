9b953fc153da1eea13053e91f508ec10ba579ac3
ballincock <zwilliams214@outlook.com>
2026-05-09 15:29:43 -0500

    Update dashboard.py

--- app/blueprints/dashboard.py
+++ app/blueprints/dashboard.py
@@ -6,6 +6,14 @@ dash_bp = Blueprint('dash', __name__)
 def index():
     if 'user_id' not in session:
         return render_template('auth.html')
+    
+    from app.models.user import User
+    user = User.query.get(session['user_id'])
+    
+    if not user:
+        session.clear() 
+        return render_template('auth.html')
+        
     return render_template('dashboard.html')
 
 @dash_bp.route('/logout')
