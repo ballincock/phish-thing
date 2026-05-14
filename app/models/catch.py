280b908687288d0e4e218e9f1615a8b94252e7f3
ballincock <zwilliams214@outlook.com>
2026-05-14 01:30:24 -0500

    Create catch.py

---
+++ app/models/catch.py
@@ -0,0 +1,13 @@
+from app.models.user import db
+from datetime import datetime
+
+class Catch(db.Model):
+    __tablename__ = 'catches'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    species = db.Column(db.String(100), nullable=False)
+    time_caught = db.Column(db.String(50))   
+    date_caught = db.Column(db.String(50))   
+    weather_conditions = db.Column(db.String(255))
+    lure_used = db.Column(db.String(255))
+    logged_at = db.Column(db.DateTime, default=datetime.utcnow)
