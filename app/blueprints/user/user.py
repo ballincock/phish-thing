c3ed9f424256a7107749d460c312e49bce7cd49b
ballincock <zwilliams214@outlook.com>
2026-05-20 13:47:37 -0500

    Update user.py

--- app/blueprints/user/user.py
+++ app/blueprints/user/user.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, session, jsonify, redirect, url_for
-from app.models.user import User, db
+from app.models.user.user import User, db
 from app.services.gallery_service import GalleryService
 
 user_bp = Blueprint('user', __name__)
