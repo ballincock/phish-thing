2ed3b40ecedc30aba0d6deeef9a1b8468af98770
ballincock <zwilliams214@outlook.com>
2026-05-21 14:17:58 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
-from app import create_app
+""" Haven't tested importing os or db in production
+"""
+import os
+from app import create_app, db
 
-app = create_app()
+app = create_app(env)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db}
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    host = os.environ.get('HOST', '127.0.0.1')
+
+    
     app.run(debug=True)
